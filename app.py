c195b0e6b447a758bbf1e236d58e5163a51b9921
fuhsienGIT <fu.hsien.loong@student.mmu.edu.my>
2024-09-27 19:34:38 +0800

    Update app.py

--- app.py
+++ app.py
@@ -35,41 +35,5 @@ app.layout = [html.H1('MCM7183 Exercise 3'),
     Input('slider-year', 'value')
 )
 
-# Create traces for each series with different styles
-trace1 = go.Bar(
-    x=df['Manga series'], 
-    y=df['Publisher'], 
-    name='Manga',
-    marker=dict(color='blue')  # Style 1: Blue bars with 'x' pattern
-)
-
-trace2 = go.Bar(
-    x=df['Manga series'], 
-    y=df['No. of collected volumes'], 
-    name='Manga',
-    marker=dict(color='green')  # Style 2: Green bars with dots pattern
-)
-
-trace3 = go.Bar(
-    x=df['Manga series'], 
-    y=df['Demographic'], 
-    name='Manga',
-    marker=dict(color='orange')  # Style 3: Orange bars with vertical line pattern
-)
-# Create the figure with all three traces (bars)
-fig = go.Figure(data=[trace1, trace2, trace3])
-
-# Customize the layout to group the bars
-fig.update_layout(
-    title='Bar Chart with 3 Different Visual Styles',
-    xaxis_title='Category',
-    yaxis_title='Values',
-    barmode='group',  # Group the bars side by side
-    template='simple_white'  # Use a simple white background for clean design
-)
-
-# Show the plot
-fig.show()
-
 if __name__ == '__main__':
     app.run(debug=True)
