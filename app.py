610e62690f6bf1da8b8e740330e3110382d1be27
fuhsienGIT <fu.hsien.loong@student.mmu.edu.my>
2024-10-07 00:01:22 +0800

    Update app.py

--- app.py
+++ app.py
@@ -34,19 +34,38 @@ app.layout = html.Div(
             dcc.Tab(label='Box Office Performance (Scatter)', value='tab-3'),
         ]),
 
+        # Dropdown for Tab 1
+        html.Div(
+            id='tab-1-dropdown',
+            children=[
+                dcc.Dropdown(
+                    id='movie-dropdown',
+                    options=[{'label': movie, 'value': movie} for movie in df['Movie']],
+                    placeholder="Select a movie",
+                    value=None,  # Default value
+                    style={'width': '50%', 'margin': 'auto'}
+                )
+            ],
+            style={'display': 'none'}  # Hidden initially
+        ),
+
         # Content area for graphs
         html.Div(id='tabs-content')
     ]
 )
 
-# Define callback to update graphs based on the selected tab and dropdown in Tab 1
+# Define callback to toggle visibility of dropdown in Tab 1 and update graphs based on selected tab
 @app.callback(
-    Output('tabs-content', 'children'),
+    [Output('tabs-content', 'children'),
+     Output('tab-1-dropdown', 'style')],
     [Input('tabs-example', 'value'),
      Input('movie-dropdown', 'value')]
 )
 def render_content(tab, selected_movie):
     if tab == 'tab-1':
+        # Dropdown is visible only on Tab 1
+        dropdown_style = {'display': 'block', 'textAlign': 'center'}
+
         # Filter data if a movie is selected in the dropdown
         filtered_df = df[df['Movie'] == selected_movie] if selected_movie else df
 
@@ -54,28 +73,25 @@ def render_content(tab, selected_movie):
         fig = px.bar(filtered_df, x='Movie', y='Rating', title='Movie Rating Distribution',
                      labels={'Rating': 'Rating Score'}, template='plotly_white')
 
-        # Dropdown for filtering by movie
-        dropdown = dcc.Dropdown(
-            id='movie-dropdown',
-            options=[{'label': movie, 'value': movie} for movie in df['Movie']],
-            placeholder="Select a movie",
-            value=None,  # Default value
-            style={'width': '50%', 'margin': 'auto'}
-        )
-
-        return html.Div([dropdown, dcc.Graph(figure=fig)])
+        return dcc.Graph(figure=fig), dropdown_style
 
     elif tab == 'tab-2':
+        # Hide dropdown for other tabs
+        dropdown_style = {'display': 'none'}
+
         # Pie chart showing the distribution of votes
         fig = px.pie(df, names='Movie', values='Votes', title='Votes Distribution',
                      labels={'Votes': 'Number of Votes'}, template='plotly_white')
-        return dcc.Graph(figure=fig)
+        return dcc.Graph(figure=fig), dropdown_style
 
     elif tab == 'tab-3':
+        # Hide dropdown for other tabs
+        dropdown_style = {'display': 'none'}
+
         # Scatter plot showing box office performance
         fig = px.scatter(df, x='Movie', y='BoxOffice', size='BoxOffice', title='Box Office Performance',
                          labels={'BoxOffice': 'Box Office (in million $)'}, template='plotly_white')
-        return dcc.Graph(figure=fig)
+        return dcc.Graph(figure=fig), dropdown_style
 
 # Run the app
 if __name__ == '__main__':
