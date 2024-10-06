0c969fdcabea26387a99f29634476606b208c507
fuhsienGIT <fu.hsien.loong@student.mmu.edu.my>
2024-10-06 23:42:53 +0800

    Update app.py

--- app.py
+++ app.py
@@ -30,8 +30,8 @@ app.layout = html.Div(
         # Tabs for different views
         dcc.Tabs(id='tabs-example', value='tab-1', children=[
             dcc.Tab(label='Rating Distribution', value='tab-1'),
-            dcc.Tab(label='Votes Count', value='tab-2'),
-            dcc.Tab(label='Box Office Performance', value='tab-3'),
+            dcc.Tab(label='Votes Distribution (Pie)', value='tab-2'),
+            dcc.Tab(label='Box Office Performance (Scatter)', value='tab-3'),
         ]),
 
         # Content area for graphs
@@ -39,32 +39,44 @@ app.layout = html.Div(
     ]
 )
 
-# Define callback to update graphs based on the selected tab
+# Define callback to update graphs based on the selected tab and dropdown in Tab 1
 @app.callback(
     Output('tabs-content', 'children'),
-    [Input('tabs-example', 'value')]
+    [Input('tabs-example', 'value'),
+     Input('movie-dropdown', 'value')]
 )
-def render_content(tab):
+def render_content(tab, selected_movie):
     if tab == 'tab-1':
+        # Filter data if a movie is selected in the dropdown
+        filtered_df = df[df['title'] == selected_movie] if selected_movie else df
+
         # Bar chart showing movie ratings
-        fig = px.bar(df, x='title', y='rating', title='Movie Rating Distribution',
+        fig = px.bar(filtered_df, x='title', y='Rating', title='Movie Rating Distribution',
                      labels={'rating': 'Rating Score'}, template='plotly_white')
-        return dcc.Graph(figure=fig)
+
+        # Dropdown for filtering by movie
+        dropdown = dcc.Dropdown(
+            id='movie-dropdown',
+            options=[{'label': movie, 'value': movie} for movie in df['title']],
+            placeholder="Select a movie",
+            value=None,  # Default value
+            style={'width': '50%', 'margin': 'auto'}
+        )
+
+        return html.Div([dropdown, dcc.Graph(figure=fig)])
 
     elif tab == 'tab-2':
-        # Line chart showing the number of votes
-        fig2 = px.pie(pie_df,labels="type",names="Continent")
-        pie_df = {'type': mylabels,'GDP': pie_data}
-
-        return dcc.Graph(figure=fig2)
+        # Pie chart showing the distribution of votes
+        fig = px.pie(df, names='Movie', values='Votes', title='Votes Distribution',
+                     labels={'type': 'Number of Votes'}, template='plotly_white')
+        return dcc.Graph(figure=fig)
 
     elif tab == 'tab-3':
-        # Scatter plot showing the box office performance
-        fig3 = px.scatter(df, x='title', y='duration', size='BoxOffice', title='Box Office Performance',
+        # Scatter plot showing box office performance
+        fig = px.scatter(df, x='Movie', y='BoxOffice', size='BoxOffice', title='Box Office Performance',
                          labels={'duration': 'Box Office (in million $)'}, template='plotly_white')
-        return dcc.Graph(figure=fig3)
+        return dcc.Graph(figure=fig)
 
 # Run the app
 if __name__ == '__main__':
     app.run_server(debug=True)
-
